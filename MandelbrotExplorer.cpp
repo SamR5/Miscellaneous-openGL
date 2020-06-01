37b2a6f0c9ced83044d41a357e3030231a370ae0
SamR5 <rami.samuel@gmx.fr>
2020-06-01 15:06:26 +0200

    rename + optimizations

--- MandelbrotExplorer.cpp
+++ MandelbrotExplorer.cpp
@@ -13,45 +13,85 @@
 #define HEIGHT 600
 #define FPS 60
 
-#define ITERATIONS_MAX 400 // for the precision
+int ITERATIONS_MAX = 64; // for the precision
 
+int iteration_required(const long double& x, const long double& y);
+void draw_set();
+void init();
 
 void timer_callback(int);
 void display_callback();
 void reshape_callback(int width, int height);
 void mouse_callback(int button, int state, int x, int y);
+void keyboard_special_callback(char key, int x, int y);
 
 // borders that will change according to the mose click
 long double Xmin, Ymin, Xmax, Ymax;
 long double deltaX, deltaY;
-float colors[ITERATIONS_MAX][3];
-
-
 
 int iteration_required(const long double& x, const long double& y) {
     // try with z = cos(z) + c & z = exp(-(z*z)) + c
-    long double za(0), zb(0), ca(x), cb(y), zaa, zbb;
+    long double za(0), zb(0), zaa, zbb;
     int counter(0);
     while ((za*za+zb*zb) < 4 && counter < ITERATIONS_MAX) {
         zaa = za;
         zbb = zb;
         za = zaa*zaa-zbb*zbb+x;
         zb = 2*zaa*zbb+y;
-        counter++;
+        ++counter;
     }
     return (counter<ITERATIONS_MAX) ? counter : 0;
 }
 
+void update_color(float h, float colors[]) {
+    // h goes from 0 to 1
+    int i = std::floor(h*6);
+    float f, q;
+    f = h*6 - i;
+    q = 1 - f;
+    switch (i%6) {
+      case 0:
+        colors[0] = 1;
+        colors[1] = f;
+        colors[2] = 0; break;
+      case 1:
+        colors[0] = q;
+        colors[1] = 1;
+        colors[2] = 0; break;
+      case 2:
+        colors[0] = 0;
+        colors[1] = 1;
+        colors[2] = f; break;
+      case 3:
+        colors[0] = 0;
+        colors[1] = q;
+        colors[2] = 1; break;
+      case 4:
+        colors[0] = f;
+        colors[1] = 0;
+        colors[2] = 1; break;
+      case 5:
+        colors[0] = 1;
+        colors[1] = 0;
+        colors[2] = q; break;
+    }
+}
+
 void draw_set() {
+    float color[3];
+    float it; // temp value for the number of iterations
+    glBegin(GL_POINTS);
     for (long double x=Xmin; x<Xmax; x+=deltaX) {
         for (long double y=Ymin; y<Ymax; y+=deltaY) {
-            int it = iteration_required(x, y);
+            it = iteration_required(x, y);
             if (it) {
-                glColor3f(colors[it][0], colors[it][1], colors[it][2]);
-                glRectd(x, y, x+deltaX, y+deltaY);
+                update_color(it/ITERATIONS_MAX, color);
+                glColor3fv(color);
+                glVertex2f(x, y);
             }
         }
     }
+    glEnd();
     // cross in the center
     glColor3f(0.0, 1.0, 0.0);
     long double Dx = Xmax-Xmin;
@@ -76,44 +116,16 @@ void init() {
     float r, g, b;
     r = g = b = 0.0;
     int index(0);
-    
-    for (; r<1; r+=5.0/ITERATIONS_MAX) {
-        colors[index][0] = r;
-        colors[index][1] = g;
-        colors[index][2] = b;
-        index++;
-    }
-    for (; g<1; g+=5.0/ITERATIONS_MAX) {
-        colors[index][0] = r;
-        colors[index][1] = g;
-        colors[index][2] = b;
-        index++;
-    }
-    for (; r>0; r-=5.0/ITERATIONS_MAX) {
-        colors[index][0] = r;
-        colors[index][1] = g;
-        colors[index][2] = b;
-        index++;
-    }
-    for (; b<1; b+=5.0/ITERATIONS_MAX) {
-        colors[index][0] = r;
-        colors[index][1] = g;
-        colors[index][2] = b;
-        index++;
-    }
-    for (; g>0 && index <= ITERATIONS_MAX; g-=5.0/ITERATIONS_MAX) {
-        colors[index][0] = r;
-        colors[index][1] = g;
-        colors[index][2] = b;
-        index++;
-    }
 }
 
-void display_callback()
-{
+void display_callback() {
+    auto start = std::chrono::high_resolution_clock::now();
     glClear(GL_COLOR_BUFFER_BIT); // clear screen
     draw_set();
     glutSwapBuffers(); // display / update
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << duration.count() << std::endl;
 }
 
 void reshape_callback(int width, int height) {
@@ -164,8 +176,18 @@ void mouse_callback(int button, int state, int x, int y) {
     glutPostRedisplay();
 }
 
-int main(int argc, char **argv)
-{
+void keyboard_callback(unsigned char key, int x, int y) {
+    if (key == '+')
+        ITERATIONS_MAX *= 2;
+    else if (key == '-')
+        ITERATIONS_MAX *= 0.5;
+    else
+        return;
+    display_callback();
+    std::cout << "iterations: " << ITERATIONS_MAX << std::endl;
+}
+
+int main(int argc, char **argv) {
     glutInit(&argc, argv); // initialize
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     //glutInitWindowPosition(); // optional
@@ -175,6 +197,7 @@ int main(int argc, char **argv)
     glutReshapeFunc(reshape_callback);
     glutTimerFunc(0, timer_callback, 0);
     glutMouseFunc(mouse_callback);
+    glutKeyboardFunc(keyboard_callback);
     init();
     glutMainLoop();
     return 0;
